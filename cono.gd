d21e61f7dde64207bca3d4a5fcd9dca1029629dc
seguramatias <segura.matias@outlook.com>
2020-02-01 12:52:29 -0300

    modificado cono

--- cono.gd
+++ cono.gd
@@ -1,6 +1,6 @@
 extends Sprite
 
-var mouseUp = false
+var mouseInArea = false
 
 var mapaPos
 func SetPosicion(pos):
@@ -10,16 +10,14 @@ func GetMapaPos():
 	return mapaPos
 
 func _process(delta):
-	if Input.is_mouse_button_pressed(1) and mouseUp:
+	if Input.is_action_just_pressed("MouseLeftClick") and mouseInArea:
 		queue_free()
 #	pass
 
-
 func _on_Area2D_mouse_entered():
-	mouseUp = true
+	mouseInArea = true
 	pass # Replace with function body.
 
-
 func _on_Area2D_mouse_exited():
-	mouseUp = false
+	mouseInArea = false
 	pass # Replace with function body.
