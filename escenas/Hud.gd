b786aba01edd3ea105a740ef5fdccc6ff6c52325
Machalord <m.machado.dev@gmail.com>
2020-02-01 15:00:25 -0300

    cambios hud

--- escenas/Hud.gd
+++ escenas/Hud.gd
@@ -8,19 +8,22 @@ extends Control
 var menu_seleccion
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Global.money
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	$money/Label.text="$"+str(Global.money)
 
 
 func _on_Button_pressed(current_panel):
 	var panels =$panels.get_children()
+	
 	for i in range($panels.get_children().size()):
 		panels[i-1].z_index=20
-		if i==current_panel:
+		get_node("buttons/buttons"+str(i+1)).visible=false
+		get_node("buttons/buttons"+str(i+1)).z_index=20
+		
+		if i==current_panel-1:
 			panels[i-1].z_index=30
+			get_node("buttons/buttons"+str(i+1)).visible=true
+			get_node("buttons/buttons"+str(i+1)).z_index=30
 	pass # Replace with function body.
---
+++ escenas/toolbutton.gd
@@ -0,0 +1,20 @@
+extends TextureButton
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+export (int) var price=0
+export (int) var tool_id=0
+export (Texture) var texture
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$label_price.text="$"+str(price)
+	$icon.texture=texture
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
