9d3af60a9acc9fd11db5e60f9e42d8660eaa63e5
seguramatias <segura.matias@outlook.com>
2020-02-01 14:55:17 -0300

    Cambios en conos, game y obrero

--- Global.gd
+++ Global.gd
@@ -6,11 +6,15 @@ extends Node
 # var b = "text"
 
 var money
+var herramienta = 4
+var actionTimer = -1
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if actionTimer >= 0:
+		actionTimer -= delta
+	pass
--- cono.gd
+++ cono.gd
@@ -2,15 +2,12 @@ extends Sprite
 
 var mouseInArea = false
 
-var mapaPos
-func SetPosicion(pos):
-	mapaPos = pos
-
-func GetMapaPos():
-	return mapaPos
+func isMouseOver():
+	return mouseInArea
 
 func _process(delta):
 	if Input.is_action_just_pressed("MouseLeftClick") and mouseInArea:
+		Global.actionTimer = 0.2
 		queue_free()
 #	pass
 
