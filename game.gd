535596a727b4aec8b350932287f1acfc8f67e0cd
seguramatias <segura.matias@outlook.com>
2020-02-01 11:09:23 -0300

    Add onClick

--- game.gd
+++ game.gd
@@ -12,6 +12,23 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	
+func _process(_delta):
 	pass
+
+func OnClick(position):
+	var mapPos = $TileMap.world_to_map(position)
+	var id = $TileMap.get_cellv(mapPos)
+	match(id):
+		1:#sSI ES UN TILE DE BACHE
+			if menuSelection == 0 and coins > 10:#SI EN EL MENU SELECCIONAMOS LA REPARACION DE BACHES
+				coins -=10
+				$TileMap.set_cellv(mapPos,tileMapSelect)
+	pass
+
+var tileMapSelect = 0
+var coins = 100
+var menuSelection = 0
+
+func _input(event):
+	if event is InputEventMouseButton:
+		self.OnClick(event.position)
