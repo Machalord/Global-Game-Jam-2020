a29c821facc93fe24d018fc2cba028768192ba69
Machalord <m.machado.dev@gmail.com>
2020-02-02 11:01:35 -0300

    autos menu

---
+++ escenas/automenu.gd
@@ -0,0 +1,37 @@
+extends Sprite
+
+var amor = 15
+var direction = true
+var andar = true
+var modelo = 0
+var explotar = false
+var pago = 0
+var dolar=preload("res://dolar.tscn")
+var pagado = false
+var subirAmor = false
+# Called when the node enters the scene tree for the first time.
+func _ready():
+
+	if direction:
+			$Area2D.add_to_group("adelanteauto")
+			$Area2D2.add_to_group("atrasauto")
+	else:
+			$Area2D.add_to_group("atrasauto")
+			$Area2D2.add_to_group("adelanteauto")
+	frame_coords=Vector2(0,modelo)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if andar:
+		$Timer.stop()
+		if direction:
+			frame_coords.x=0
+			position.x -= 1.28 *5
+			position.y += 0.64 *5
+		else:
+			frame_coords.x=1
+			position.x += 1.28 *5
+			position.y -= 0.64 *5
+#	pass
